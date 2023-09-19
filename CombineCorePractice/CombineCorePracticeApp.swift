1aa7b40efaf705484bd1e833d30329f51f13b3bf
Daniel Moore <dan@mooresolutions.io>
2023-09-19 00:55:43 -0500

    Initial Commit

---
+++ CombineCorePractice/CombineCorePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineCorePracticeApp.swift
+//  CombineCorePractice
+//
+//  Created by Daniel Moore on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineCorePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
